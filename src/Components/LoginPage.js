d1f4c5bb5491183ca31a60df1ddcd144aca29c3b
Ankit Burman <ank2911it@gmail.com>
2022-03-25 12:09:41 +0530

    Component Added

---
+++ src/Components/LoginPage.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+
+function LoginPage(props) {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    console.log(`EMAIL: ${email}`);
+    console.log(`PASS: ${pass}`);
+
+    const userlogininfo = {
+      useremail: email,
+      userpass: pass
+    }
+
+
+    setEmail('')
+    setPass('')
+  }
+
+  return (
+    <div className='container' style={{padding:'150px',width:'600px'}}>
+      <br /> 
+      <h3 style={{textAlign:'center', color:'green'}}>USER LOGIN</h3>
+      <br /> <br /> <br />
+      <form onSubmit={handleSubmit}>
+        <input type="email"
+        className="form-control form-control-sm"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="ENTER EMAIL"
+          required />
+             <br />
+
+        <input type="password"
+        className="form-control form-control-sm"
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          placeholder="ENTER PASSWORD"
+          required />
+        <br />
+        <input type="submit" value="LOGIN" className="btn btn-success" />
+      </form>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ src/Components/SignUpForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+
+function SignUpForm() {
+
+  const [name, setName] = useState("");
+  const [isError, setIsError] = useState("");
+  const [email, setEmail] = useState("");; 
+  const [pass, setPass] = useState("");
+  const [confpass,setconfPass] = useState("");
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(`Registered Successfully:`);
+    console.log(`NAME: ${name}`);
+    console.log(`EMAIL: ${email}`);
+
+    const userinfo = {
+        username: name,
+        useremail: email,
+        userpass: pass,
+        userconfpass: confpass       
+    }
+
+    setName('')
+    setEmail('')
+    setPass('')
+    setconfPass('')
+
+}
+const checkpassword = (e) => {
+  setconfPass(e.target.value);
+  if(pass !== confpass){
+    setIsError("Password is not Matched");
+  }
+  else{
+    setIsError("");
+  }
+}
+
+
+  return (
+    <div style={{padding:'5px',width:'300px', margin: 'auto'}}>
+      
+        <h3 style={{padding:'5px',color:'blue'}}> SignUp</h3>
+        <br/>
+      <form onSubmit={handleSubmit}>
+    <label>Name:</label>
+      <input className="form-control form-control-sm" type="text" value={name}
+                    onChange={(e) => setName(e.target.value)} placeholder="Enter Full Name" required/>
+
+      <label>Email:</label>
+      <input className="form-control form-control-sm" type="email"  value={email}
+                    onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email"  required/>
+
+      <label>Create Password:</label>
+      <input className="form-control form-control-sm" type="Password"  value={pass}
+                    onChange={(e) => setPass(e.target.value)} placeholder="Enter Password"  required/>
+     
+     <label>Confirm Password:</label>
+      <input className="form-control form-control-sm" type="Password"  value={confpass}
+                    onChange={(e) => checkpassword(e)} placeholder="Confirm Password"  required/>
+     <div style={{color:'red'}}>
+      {isError}
+     </div>
+     <br></br>
+      <button type="submit" className="btn btn-primary">Sign Up</button>
+      </form>
+    </div>
+  )
+}
+
+export default SignUpForm
